ffaca6cbbfd7642584e06ba468c86ea7c36106f7
MarcosJRS <marcosjrs80@gmail.com>
2022-06-01 13:32:10 +0200

    Creacion de "pagina" (modulo) proyectos, dentro de carpeta pages que contendrá su propio routing, con:   ng g m pages/proyectos --routing

---
+++ src/app/pages/proyectos/proyectos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProyectosRoutingModule } from './proyectos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProyectosRoutingModule
+  ]
+})
+export class ProyectosModule { }
