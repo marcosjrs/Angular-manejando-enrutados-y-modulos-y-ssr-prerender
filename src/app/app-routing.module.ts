4acf65eadbd40045b7ab09f386089cbd005a5544
MarcosJRS <marcosjrs80@gmail.com>
2022-06-01 16:11:52 +0200

    Eliminación de todo lo de pages, para empezar de nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path :"", 
-    loadChildren: ()=>import('./pages/pages.module').then((module)=>module.PagesModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
